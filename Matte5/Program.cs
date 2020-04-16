9d8e4ef24320251eab922deec13d31495ab4c19b
Tllija <badlands.hugo@gmail.com>
2020-04-16 20:42:58 +0200

    whoops, forgot to make it recursive!

--- Matte5/Program.cs
+++ Matte5/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Flee.PublicTypes;
+using System;
+using System.Collections.Generic;
 
 namespace Matte5
 {
@@ -6,27 +8,44 @@ namespace Matte5
     {
         static void Main()
         {
-            Console.WriteLine("Press enter after every pramter\nEnter n, A0, C0");
+            while (true)
+            {
+                Console.WriteLine("Press enter after every pramter\nEnter n, A0, func for A, C0, func for C");
 
-            var n = int.Parse(Console.ReadLine());
+                var n = int.Parse(Console.ReadLine());
 
-            var a0= Double.Parse(Console.ReadLine());
-            Func<double, int, double> aLambda = (a0, n) => a0 * 0.75; //Console.ReadLine();
+                var a0 = Double.Parse(Console.ReadLine());
+                Func<double, double, double> aLambda = ParseString(Console.ReadLine());
 
-            var c0 = Double.Parse(Console.ReadLine());
-            Func<double, int, double> cLambda = (c0, n) => c0 * 0.7; //Console.ReadLine(); 
+                var c0 = Double.Parse(Console.ReadLine());
+                Func<double, double, double> cLambda = ParseString(Console.ReadLine());
 
-            var (an, cn) = iter(n, a0, aLambda, c0, cLambda);
-            Console.WriteLine("a: {0}, c: {1}", an, cn);
-
+                var (an, cn) = iter(n, a0, aLambda, c0, cLambda);
+                Console.WriteLine("a: {0}, c: {1}", an, cn);
+            }
+            
         }
 
-        private Func<double, int, double> ParseString(string str)
+        private static Func<double, double, double> ParseString(string str)
         {
-            throw new NotImplementedException();
+            var context = new ExpressionContext();
+            context.Imports.AddType(typeof(Math));
+            context.Variables.Add("x", (double)0.0);
+            context.Variables.Add("n", (double)0);
+            var e = context.CompileDynamic(str);
+
+            Func<double, double, double> func = (double x, double n) =>
+            {
+                context.Variables["x"] = x;
+                context.Variables["n"] = n;
+                var result = (double)e.Evaluate();
+                return result;
+            };
+
+            return func;
         }
 
-        public static (double, double) iter(int n, double a0, Func<double, int, double> af, double c0, Func<double, int, double> cf)
+        public static (double, double) iter(int n, double a0, Func<double, double, double> af, double c0, Func<double, double, double> cf)
         {
             switch (n)
             {
@@ -38,7 +57,7 @@ namespace Matte5
                     var an = a0 - aCarsRented * 0.75 + cCarsRented * 0.3; // A at n+1
                     var cn = c0 - cCarsRented * 0.3 + aCarsRented * 0.75; // C at n+1
 
-                    return (an, cn);
+                    return iter(n + 1, an, af, cn, cf);
             }
         }
     }
