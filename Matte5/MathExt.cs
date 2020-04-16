e99371ecef6e48eb255eb14a8213f6128917259c
Tllija <badlands.hugo@gmail.com>
2020-04-16 22:29:01 +0200

    Added clamp function, moved over from recursion without tail call optimization to a loop, and ignore the rest.

---
+++ Matte5/MathExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matte5
+{
+    public static class MathExt
+    {
+        public static double Clamp(this double n, double min, double max)
+        {
+            return Math.Clamp(n, min, max);
+        }
+
+        public static double Round(this double n, byte decimals)
+        {
+            return Math.Round(n, decimals);
+        }
+    }
+}
--- Matte5/Program.cs
+++ Matte5/Program.cs
@@ -10,7 +10,7 @@ namespace Matte5
         {
             while (true)
             {
-                Console.WriteLine("Press enter after every pramter\nEnter n, A0, func for A, C0, func for C");
+                Console.WriteLine("\n\nPress enter after every pramter\nEnter n, A0, func for A, C0, func for C");
 
                 var n = int.Parse(Console.ReadLine());
 
@@ -20,7 +20,9 @@ namespace Matte5
                 var c0 = Double.Parse(Console.ReadLine());
                 Func<double, double, double> cLambda = ParseString(Console.ReadLine());
 
-                var (an, cn) = iter(n, a0, aLambda, c0, cLambda);
+                //var (an, cn) = iter(n, a0, aLambda, c0, cLambda);
+                Console.WriteLine("Calculatig for n = {0}, A0 = {1}, C0 = {2}", n, a0, c0);
+                var (an, cn) = calc(n, a0, aLambda, c0, cLambda, true);
                 Console.WriteLine("a: {0}, c: {1}", an, cn);
             }
             
@@ -45,6 +47,39 @@ namespace Matte5
             return func;
         }
 
+        // Normal loop, this performs much better due to not having to have a bazillion stack frames 
+        public static (double, double) calc(int n,
+            double a0, Func<double, double, double> af,
+            double c0, Func<double, double, double> cf,
+            bool clamp = false)
+        {
+
+            double a = a0, c = c0;
+            for(int i = 0; i < n; i++)
+            {
+                var aRented = af(a, i);
+                var cRented = cf(c, i);
+
+                if (clamp)
+                {
+                    // Negative cars cannot be rented and more cars than exist cannot be rented.
+                    aRented = aRented.Clamp(0, a);
+                    cRented = cRented.Clamp(0, c);
+                }
+
+                var an = a - aRented * 0.75 + cRented * 0.30;
+                var cn = c - cRented * 0.30 + aRented * 0.75;
+
+                a = an;
+                c = cn;
+
+                Console.WriteLine("N: {0} => ({1}, {2}) where the amount rented was ({3}, {4})", i, a.Round(2), c.Round(2), aRented.Round(2), cRented.Round(2));
+            }
+
+            return (a, c);
+        }
+
+        [Obsolete]
         public static (double, double) iter(int n, double a0, Func<double, double, double> af, double c0, Func<double, double, double> cf)
         {
             switch (n)
@@ -57,6 +92,8 @@ namespace Matte5
                     var an = a0 - aCarsRented * 0.75 + cCarsRented * 0.3; // A at n+1
                     var cn = c0 - cCarsRented * 0.3 + aCarsRented * 0.75; // C at n+1
 
+                    // unfortunately, C# doesn't support tail call optimization,
+                    //so this will only work for very, very, very small values of n, a0, and c0.
                     return iter(n + 1, an, af, cn, cf);
             }
         }
